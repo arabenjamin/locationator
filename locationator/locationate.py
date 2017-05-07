7237eeb423718d32e6506e92a97c8704158f5b27
arabenjamin <ara.benjamin@gmail.com>
2017-05-06 20:29:30 -0700

    removed config dependency

--- locationator/locationate.py
+++ locationator/locationate.py
@@ -50,5 +50,5 @@ class Locationator():
 		
 if __name__ == "__main__":
 	address = "3009 NE 120th St, Seattle Wa, 98125"
-	location = Locationator(l)
+	location = Locationator()
 	location.geocode(address)
