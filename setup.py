dbde23a01a1216d7a5f52284b3f4a18e2f3f938b
Arabenjamin <arabenjamin@laptop.local>
2019-06-17 09:16:04 -0700

    fixed install_requires keyword parameter

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
-from locationator import __version__
+#from locationator import __version__
 from setuptools import setup
 
-install_requires = open("requirements.txt").read().split('\n')
+__version__ = '0.1.2'
+
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
 setup(
 	name='locationator',
+	install_requires=install_requires,
 	version= __version__,
-	description='simple loaction tool',
+	description='simple location tool',
 	author='Ara Benjamin',
 	author_email='ara.benjamin@gmail.com',
 	url= "https://github.com/arabenjamin/locationator",
 	license='Creative Commons Attribution-Noncommercial-Share Alike license',
 	packages=['locationator'],
-	install_requires=install_requires,
 	classifiers=['Programming Language :: Python :: 2.7'],
 	zip_safe=False)
